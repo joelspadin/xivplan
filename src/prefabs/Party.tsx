09e4259a333aefc904d045c284f9a6fbbdc4740b
Joel Spadin <joelspadin@gmail.com>
2021-11-26 13:48:23 -0600

    Add party variant selector

--- src/prefabs/Party.tsx
+++ src/prefabs/Party.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@fluentui/react';
+import { IButtonStyles, IconButton, IStackTokens, Label, Stack } from '@fluentui/react';
 import * as React from 'react';
 import { useCallback } from 'react';
 import { Group, Image, Rect } from 'react-konva';
@@ -20,11 +20,86 @@ import { ResizeableObjectContainer } from './ResizeableObjectContainer';
 
 const DEFAULT_SIZE = 32;
 
-function makeIcon(name: string, icon: string) {
+interface JobProps {
+    name: string;
+    icon: string;
+}
+
+enum Job {
+    RoleAny,
+    RoleTank,
+    RoleHealer,
+    RoleDps,
+    RoleMelee,
+    RoleRanged,
+    RoleMagicRanged,
+    RolePhysicalRanged,
+    Paladin,
+    Warrior,
+    DarkKnight,
+    Gunbreaker,
+    WhiteMage,
+    Scholar,
+    Astrologian,
+    Monk,
+    Dragoon,
+    Ninja,
+    Samurai,
+    Bard,
+    Machinist,
+    Dancer,
+    BlackMage,
+    Summoner,
+    RedMage,
+}
+
+const JOBS: Record<Job, JobProps> = {
+    [Job.RoleAny]: { name: 'Any player', icon: 'any.png' },
+    [Job.RoleTank]: { name: 'Tank', icon: 'tank.png' },
+    [Job.RoleHealer]: { name: 'Healer', icon: 'healer.png' },
+    [Job.RoleDps]: { name: 'DPS', icon: 'dps.png' },
+    [Job.RoleMelee]: { name: 'Melee DPS', icon: 'melee.png' },
+    [Job.RoleRanged]: { name: 'Ranged DPS', icon: 'ranged.png' },
+    [Job.RoleMagicRanged]: { name: 'Magic Ranged DPS', icon: 'magic_ranged.png' },
+    [Job.RolePhysicalRanged]: { name: 'Physical Ranged DPS', icon: 'physical_ranged.png' },
+    [Job.Paladin]: { name: 'Paladin', icon: 'PLD.png' },
+    [Job.Warrior]: { name: 'Warrior', icon: 'WAR.png' },
+    [Job.DarkKnight]: { name: 'Dark Knight', icon: 'DRK.png' },
+    [Job.Gunbreaker]: { name: 'Gunbreaker', icon: 'GNB.png' },
+    [Job.WhiteMage]: { name: 'White Mage', icon: 'WHM.png' },
+    [Job.Scholar]: { name: 'Scholar', icon: 'SCH.png' },
+    [Job.Astrologian]: { name: 'Astrologian', icon: 'AST.png' },
+    [Job.Monk]: { name: 'Monk', icon: 'MNK.png' },
+    [Job.Dragoon]: { name: 'Dragoon', icon: 'DRG.png' },
+    [Job.Ninja]: { name: 'Ninja', icon: 'NIN.png' },
+    [Job.Samurai]: { name: 'Samurai', icon: 'SAM.png' },
+    [Job.Bard]: { name: 'Bard', icon: 'BRD.png' },
+    [Job.Machinist]: { name: 'Machinist', icon: 'MCH.png' },
+    [Job.Dancer]: { name: 'Dancer', icon: 'DNC.png' },
+    [Job.BlackMage]: { name: 'Black Mage', icon: 'BLM.png' },
+    [Job.Summoner]: { name: 'Summoner', icon: 'SMN.png' },
+    [Job.RedMage]: { name: 'Red Mage', icon: 'RDM.png' },
+};
+
+function getJob(job: Job) {
+    const props = JOBS[job];
+    if (!props) {
+        throw new Error('Unknown job');
+    }
+    return props;
+}
+
+function getIconUrl(icon: string) {
+    return new URL(`../assets/actor/${icon}`, import.meta.url).toString();
+}
+
+function makeIcon(job: Job) {
+    const { icon, name } = getJob(job);
+
     // eslint-disable-next-line react/display-name
     return () => {
         const [, setDragObject] = usePanelDrag();
-        const iconUrl = new URL(`../assets/actor/${icon}`, import.meta.url).toString();
+        const iconUrl = getIconUrl(icon);
 
         return (
             <PrefabIcon
@@ -94,6 +169,59 @@ const PartyDetails: React.FC<ListComponentProps<PartyObject>> = ({ object, isNes
 
 registerListComponent<PartyObject>(ObjectType.Party, PartyDetails);
 
+const iconChoices = [
+    [Job.RoleAny, Job.RoleTank, Job.RoleHealer, Job.RoleDps],
+    [Job.RoleMagicRanged, Job.RolePhysicalRanged, Job.RoleRanged, Job.RoleMelee],
+    [Job.Paladin, Job.Warrior, Job.DarkKnight, Job.Gunbreaker],
+    [Job.WhiteMage, Job.Scholar, Job.Astrologian],
+    [Job.Monk, Job.Dragoon, Job.Ninja, Job.Samurai],
+    [Job.Bard, Job.Machinist, Job.Dancer, Job.BlackMage, Job.Summoner, Job.RedMage],
+].map((row) => row.map((job) => getJob(job)));
+
+const buttonStyles: IButtonStyles = {
+    icon: {
+        fontSize: 32,
+        height: 32,
+        lineHeight: 32,
+    },
+};
+
+const stackTokens: IStackTokens = {
+    childrenGap: 2,
+    padding: '1px 0',
+};
+
+const PartyIconSelect: React.FC<PropertiesControlProps<PartyObject>> = ({ object }) => {
+    const [, dispatch] = useScene();
+
+    const onClick = useCallback(
+        (name: string, image: string) => dispatch({ type: 'update', value: { ...object, name, image } }),
+        [dispatch, object],
+    );
+
+    return (
+        <div>
+            <Label>Variant</Label>
+            {iconChoices.map((row, i) => (
+                <Stack horizontal key={i} tokens={stackTokens}>
+                    {row.map((job, j) => {
+                        const icon = getIconUrl(job.icon);
+                        return (
+                            <IconButton
+                                key={j}
+                                title={job.name}
+                                iconProps={{ imageProps: { src: icon } }}
+                                styles={buttonStyles}
+                                onClick={() => onClick(job.name, icon)}
+                            />
+                        );
+                    })}
+                </Stack>
+            ))}
+        </div>
+    );
+};
+
 const PartyEditControl: React.FC<PropertiesControlProps<PartyObject>> = ({ object }) => {
     const [, dispatch] = useScene();
 
@@ -106,41 +234,40 @@ const PartyEditControl: React.FC<PropertiesControlProps<PartyObject>> = ({ objec
         <Stack>
             <DeferredTextField label="Name" value={object.name} onChange={onNameChanged} />
             <ImageObjectProperties object={object} />
+            <PartyIconSelect object={object} />
         </Stack>
     );
 };
 
 registerPropertiesControl<PartyObject>(ObjectType.Party, PartyEditControl);
 
-export const PartyTank = makeIcon('Tank', 'tank.png');
-export const PartyHealer = makeIcon('Healer', 'healer.png');
-export const PartyTankOrHealer = makeIcon('Tank/healer', 'tank_or_healer.png');
-export const PartyDps = makeIcon('DPS', 'dps.png');
-export const PartyAny = makeIcon('Any player', 'any.png');
+export const PartyAny = makeIcon(Job.RoleAny);
+export const PartyTank = makeIcon(Job.RoleTank);
+export const PartyHealer = makeIcon(Job.RoleHealer);
+export const PartyDps = makeIcon(Job.RoleDps);
 
-export const PartyMelee = makeIcon('Melee DPS', 'melee.png');
-export const PartyRanged = makeIcon('Ranged DPS', 'ranged.png');
-export const PartyMagicRanged = makeIcon('Magic ranged DPS', 'magic_ranged.png');
-export const PartyPhysicalRanged = makeIcon('Physical ranged DPS', 'physical_ranged.png');
+export const PartyMelee = makeIcon(Job.RoleMelee);
+export const PartyRanged = makeIcon(Job.RoleRanged);
+export const PartyMagicRanged = makeIcon(Job.RoleMagicRanged);
+export const PartyPhysicalRanged = makeIcon(Job.RolePhysicalRanged);
 
-export const PartyPaladin = makeIcon('Paladin', 'PLD.png');
-export const PartyWarrior = makeIcon('Warrior', 'WAR.png');
-export const PartyDarkKnight = makeIcon('Dark Knight', 'DRK.png');
-export const PartyGunbreaker = makeIcon('Gunbreaker', 'GNB.png');
+export const PartyPaladin = makeIcon(Job.Paladin);
+export const PartyWarrior = makeIcon(Job.Warrior);
+export const PartyDarkKnight = makeIcon(Job.DarkKnight);
+export const PartyGunbreaker = makeIcon(Job.Gunbreaker);
 
-export const PartyWhiteMage = makeIcon('White Mage', 'WHM.png');
-export const PartyScholar = makeIcon('Scholar', 'SCH.png');
-export const PartyAstrologian = makeIcon('Astrologian', 'AST.png');
+export const PartyWhiteMage = makeIcon(Job.WhiteMage);
+export const PartyScholar = makeIcon(Job.Scholar);
+export const PartyAstrologian = makeIcon(Job.Astrologian);
 
-export const PartyMonk = makeIcon('Monk', 'MNK.png');
-export const PartyDragoon = makeIcon('Dragoon', 'DRG.png');
-export const PartyNinja = makeIcon('Ninja', 'NIN.png');
-export const PartySamurai = makeIcon('Samurai', 'SAM.png');
+export const PartyMonk = makeIcon(Job.Monk);
+export const PartyDragoon = makeIcon(Job.Dragoon);
+export const PartyNinja = makeIcon(Job.Ninja);
+export const PartySamurai = makeIcon(Job.Samurai);
 
-export const PartyBard = makeIcon('Bard', 'BRD.png');
-export const PartyMachinist = makeIcon('Machinist', 'MCH.png');
-export const PartyDancer = makeIcon('Dancer', 'DNC.png');
-
-export const PartyBlackMage = makeIcon('Black Mage', 'BLM.png');
-export const PartySummoner = makeIcon('Summoner', 'SMN.png');
-export const PartyRedMage = makeIcon('Red Mage', 'RDM.png');
+export const PartyBard = makeIcon(Job.Bard);
+export const PartyMachinist = makeIcon(Job.Machinist);
+export const PartyDancer = makeIcon(Job.Dancer);
+export const PartyBlackMage = makeIcon(Job.BlackMage);
+export const PartySummoner = makeIcon(Job.Summoner);
+export const PartyRedMage = makeIcon(Job.RedMage);
