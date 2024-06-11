a276d8858b5051ceea9e7879fddf52f485f8cac1
Joel Spadin <joelspadin@gmail.com>
2024-06-10 19:21:55 -0500

    Allow installing as a PWA
    The .xivplan file handler isn't implemented yet.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,61 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            strategies: 'generateSW',
+            registerType: 'prompt',
+            injectRegister: false,
+            pwaAssets: { disabled: false, config: true, htmlPreset: '2023', overrideManifestIcons: true },
+            manifest: {
+                name: 'XIVPlan',
+                short_name: 'XIVPlan',
+                description: 'FFXIV raid planner',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+                file_handlers: [
+                    {
+                        action: '/',
+                        accept: {
+                            'application/vnd.xivplan.plan+json': ['.xivplan'],
+                        },
+                    },
+                ],
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
+            injectManifest: {
+                globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+            },
+        }),
+    ],
 });
