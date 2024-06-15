fc0ae2cc46d718b342c2c0e461a36e319fd97750
Joel Spadin <joelspadin@gmail.com>
2024-06-14 22:02:25 -0500

    Migrate most of arena panel

--- src/panel/ArenaPanel.tsx
+++ src/panel/ArenaPanel.tsx
@@ -1,75 +1,41 @@
+import { INavLink, INavLinkGroup, Nav } from '@fluentui/react';
 import {
-    classNamesFunction,
-    DefaultButton,
-    DialogFooter,
-    IModalProps,
-    IModalStyles,
-    INavLink,
-    INavLinkGroup,
-    IStackTokens,
-    IStyle,
-    IStyleFunction,
-    IStyleFunctionOrObject,
-    mergeStyleSets,
-    Nav,
-    PrimaryButton,
-    Separator,
-    Stack,
-    Theme,
-    useTheme,
-} from '@fluentui/react';
-import { Button } from '@fluentui/react-components';
-import { useBoolean } from '@fluentui/react-hooks';
+    Button,
+    Dialog,
+    DialogActions,
+    DialogBody,
+    DialogContent,
+    DialogSurface,
+    DialogTitle,
+    DialogTrigger,
+    Divider,
+    makeStyles,
+    mergeClasses,
+    tokens,
+    typographyStyles,
+} from '@fluentui/react-components';
 import React, { MouseEventHandler, useCallback, useState } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
-import { BaseDialog, IBaseDialogStyles } from '../BaseDialog';
+import { useScene } from '../SceneProvider';
 import { ARENA_PRESETS } from '../presets/ArenaPresets';
 import { ScenePreview } from '../render/SceneRenderer';
 import { ArenaPreset, Scene } from '../scene';
-import { useScene } from '../SceneProvider';
 import { ArenaBackgroundEdit } from './ArenaBackgroundEdit';
 import { ArenaGridEdit } from './ArenaGridEdit';
 import { ArenaShapeEdit } from './ArenaShapeEdit';
 import { PANEL_PADDING } from './PanelStyles';
 
-const stackTokens: IStackTokens = {
-    childrenGap: 10,
-};
-
-const classNames = mergeStyleSets({
-    panel: {
-        padding: PANEL_PADDING,
-    } as IStyle,
-    list: {
-        padding: 0,
-        overflow: 'auto',
-        display: 'flex',
-        flexFlow: 'row wrap',
-        maxHeight: 'calc(80vh - 80px)',
-        gap: '20px',
-    } as IStyle,
-});
-
 export const ArenaPanel: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <Stack className={classNames.panel} tokens={stackTokens}>
+        <div className={classes.panel}>
             <ArenaShapeEdit />
             <ArenaGridEdit />
             <ArenaBackgroundEdit />
-            <Separator />
+            <Divider />
             <SelectPresetButton />
-        </Stack>
-    );
-};
-
-const SelectPresetButton: React.FC = () => {
-    const [isOpen, { setTrue: showDialog, setFalse: hideDialog }] = useBoolean(false);
-
-    return (
-        <>
-            <Button onClick={showDialog}>Select preset</Button>
-            <SelectPresetDialog isOpen={isOpen} onDismiss={hideDialog} />
-        </>
+        </div>
     );
 };
 
@@ -86,6 +52,8 @@ function getPresetsForKey(key: string | undefined) {
     return ARENA_PRESETS[key1 ?? '']?.[key2 ?? ''] ?? [];
 }
 
+// TODO: migrate to Tree or wait for Nav component to be implement in v9
+// https://github.com/orgs/microsoft/projects/786/views/1?pane=issue&itemId=24403433
 const navLinkGroups: INavLinkGroup[] = [
     ...Object.entries(ARENA_PRESETS).map(([key, value]) => {
         return {
@@ -97,10 +65,10 @@ const navLinkGroups: INavLinkGroup[] = [
 
 const PREVIEW_SIZE = 240;
 
-// TODO: limit nav height to ~80vh and scroll
-
-const SelectPresetDialog: React.FC<IModalProps> = (props) => {
+const SelectPresetButton: React.FC = () => {
+    const classes = useStyles();
     const { dispatch } = useScene();
+    const [open, setOpen] = useState(false);
     const [key, setKey] = useState(navLinkGroups[0]?.links[0]?.key);
     const [selected, setSelected] = useState<ArenaPreset>();
 
@@ -109,9 +77,9 @@ const SelectPresetDialog: React.FC<IModalProps> = (props) => {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const { name, ...arena } = preset;
             dispatch({ type: 'arena', value: arena });
-            props.onDismiss?.();
+            setOpen(false);
         },
-        [dispatch, props],
+        [dispatch, setOpen],
     );
 
     useHotkeys(
@@ -127,96 +95,58 @@ const SelectPresetDialog: React.FC<IModalProps> = (props) => {
     const presets = getPresetsForKey(key);
 
     return (
-        <BaseDialog headerText="Arena presets" {...props} dialogStyles={dialogStyles} styles={modalStyles}>
-            <Stack horizontal tokens={{ childrenGap: 20 }}>
-                <Nav
-                    groups={navLinkGroups}
-                    selectedKey={key}
-                    onLinkClick={(ev, item) => {
-                        if (item) {
-                            if (item.links) {
-                                return;
-                            }
+        <Dialog open={open} onOpenChange={(ev, data) => setOpen(data.open)}>
+            <DialogTrigger>
+                <Button>Select preset</Button>
+            </DialogTrigger>
+            <DialogSurface className={classes.dialogSurface}>
+                <DialogBody>
+                    <DialogTitle>Arena presets</DialogTitle>
+                    <DialogContent className={classes.dialogContent}>
+                        <div className={classes.nav}>
+                            <Nav
+                                groups={navLinkGroups}
+                                selectedKey={key}
+                                onLinkClick={(ev, item) => {
+                                    if (item) {
+                                        if (item.links) {
+                                            return;
+                                        }
 
-                            setKey(item.key);
-                            setSelected(undefined);
-                        }
-                    }}
-                />
-                <div>
-                    <ul className={classNames.list}>
-                        {presets?.map((preset) => (
-                            <PresetItem
-                                key={preset.name}
-                                preset={preset}
-                                selected={preset === selected}
-                                onClick={() => setSelected(preset)}
-                                onDoubleClick={() => applyPreset(preset)}
+                                        setKey(item.key);
+                                        setSelected(undefined);
+                                    }
+                                }}
                             />
-                        ))}
-                    </ul>
-                </div>
-            </Stack>
-            <DialogFooter>
-                <PrimaryButton
-                    text="Select preset"
-                    disabled={!selected}
-                    onClick={() => selected && applyPreset(selected)}
-                />
-                <DefaultButton text="Cancel" onClick={() => props.onDismiss?.()} />
-            </DialogFooter>
-        </BaseDialog>
+                        </div>
+                        <ul className={classes.presetList}>
+                            {presets?.map((preset) => (
+                                <PresetItem
+                                    key={preset.name}
+                                    preset={preset}
+                                    selected={preset === selected}
+                                    onClick={() => setSelected(preset)}
+                                    onDoubleClick={() => applyPreset(preset)}
+                                />
+                            ))}
+                        </ul>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            appearance="primary"
+                            disabled={!selected}
+                            onClick={() => selected && applyPreset(selected)}
+                        >
+                            Select preset
+                        </Button>
+                        <DialogTrigger>
+                            <Button>Cancel</Button>
+                        </DialogTrigger>
+                    </DialogActions>
+                </DialogBody>
+            </DialogSurface>
+        </Dialog>
     );
-};
-
-const modalStyles: Partial<IModalStyles> = {
-    root: {
-        paddingTop: 40,
-        alignItems: 'start',
-    },
-};
-
-const dialogStyles: IStyleFunctionOrObject<Theme, IBaseDialogStyles> = {
-    body: {
-        width: 1000,
-
-        '@media (max-width: 1200px)': {
-            width: 800,
-        } as IStyle,
-
-        '@media (max-width: 900px)': {
-            width: 'calc(100% - 50px)',
-        } as IStyle,
-    },
-};
-
-interface IPresetStyles {
-    item: IStyle;
-    selected: IStyle;
-    header: IStyle;
-}
-
-const getPresetClassNames = classNamesFunction<Theme, IPresetStyles>();
-
-const getPresetStyles: IStyleFunction<Theme, IPresetStyles> = (theme) => {
-    return {
-        item: {
-            width: PREVIEW_SIZE + 2,
-            listStyle: 'none',
-            boxSizing: 'border-box',
-            border: '1px solid transparent',
-        },
-        selected: {
-            borderColor: theme.palette.themePrimary,
-        },
-        header: {
-            textAlign: 'center',
-            marginBottom: -10,
-            paddingInlineStart: theme.spacing.s1,
-            paddingInlineEnd: theme.spacing.s1,
-            ...theme.fonts.mediumPlus,
-        },
-    };
 };
 
 interface PresetItemProps {
@@ -227,10 +157,7 @@ interface PresetItemProps {
 }
 
 const PresetItem: React.FC<PresetItemProps> = ({ preset, selected, onClick, onDoubleClick }) => {
-    const theme = useTheme();
-    const classNames = getPresetClassNames(getPresetStyles, theme);
-
-    const className = `${classNames.item} ${selected ? classNames.selected : ''}`;
+    const classes = useStyles();
 
     const scene: Scene = {
         nextId: 0,
@@ -239,11 +166,102 @@ const PresetItem: React.FC<PresetItemProps> = ({ preset, selected, onClick, onDo
     };
 
     return (
-        <li className={className} onClick={onClick} onDoubleClick={onDoubleClick}>
-            <Stack>
-                <div className={classNames.header}>{preset.name}</div>
-                <ScenePreview scene={scene} width={PREVIEW_SIZE} height={PREVIEW_SIZE} />
-            </Stack>
+        <li
+            className={mergeClasses(classes.presetItem, selected && classes.selected)}
+            onClick={onClick}
+            onDoubleClick={onDoubleClick}
+        >
+            <div className={classes.presetHeader}>{preset.name}</div>
+            <ScenePreview scene={scene} width={PREVIEW_SIZE} height={PREVIEW_SIZE} />
         </li>
     );
 };
+
+const useStyles = makeStyles({
+    panel: {
+        display: 'flex',
+        flexFlow: 'column',
+        rowGap: tokens.spacingVerticalM,
+        padding: `${PANEL_PADDING}px`,
+    },
+
+    dialogSurface: {
+        maxWidth: '1080px',
+
+        '@media (max-width: 1200px)': {
+            maxWidth: '800px',
+        },
+
+        '@media (max-width: 900px)': {
+            maxWidth: 'calc(100% - 50px)',
+        },
+    },
+
+    dialogContent: {
+        display: 'flex',
+        flexFlow: 'row',
+
+        height: '80vh',
+        paddingBottom: tokens.spacingVerticalL,
+    },
+
+    nav: {
+        minWidth: '200px',
+        marginRight: tokens.spacingHorizontalXS,
+        overflowY: 'auto',
+    },
+
+    presetList: {
+        margin: 0,
+        padding: `${tokens.spacingVerticalL} ${tokens.spacingHorizontalL}`,
+
+        overflowY: 'auto',
+        display: 'flex',
+        flexFlow: 'row wrap',
+        alignItems: 'start',
+        alignContent: 'start',
+        justifyContent: 'space-between',
+
+        width: '100%',
+        gap: '20px',
+
+        backgroundColor: tokens.colorNeutralBackground2,
+        borderRadius: tokens.borderRadiusLarge,
+    },
+
+    presetItem: {
+        display: 'flex',
+        flexFlow: 'column',
+
+        width: `${PREVIEW_SIZE + 2}px`,
+        listStyle: 'none',
+        boxSizing: 'border-box',
+        border: `${tokens.strokeWidthThin} solid transparent`,
+        borderRadius: tokens.borderRadiusLarge,
+
+        transitionProperty: 'background, border, color',
+        transitionDuration: tokens.durationFaster,
+        transitionTimingFunction: tokens.curveEasyEase,
+
+        ':hover': {
+            backgroundColor: tokens.colorSubtleBackgroundHover,
+        },
+        ':hover:active': {
+            backgroundColor: tokens.colorSubtleBackgroundPressed,
+        },
+    },
+    selected: {
+        backgroundColor: tokens.colorSubtleBackgroundSelected,
+        // TODO: should be able to just set backgroundColor here, but its type is "undefined"?
+        border: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralForeground2BrandSelected}`,
+        boxShadow: tokens.shadow4,
+    },
+    presetHeader: {
+        textAlign: 'center',
+        marginTop: tokens.spacingVerticalS,
+        marginBottom: '-10px',
+        paddingInlineStart: tokens.spacingHorizontalS,
+        paddingInlineEnd: tokens.spacingHorizontalS,
+        ...typographyStyles.body2,
+    },
+});
