afa54249c8f311ea2e22614ef34e2713c6f721b7
Joel Spadin <joelspadin@gmail.com>
2025-04-28 18:15:34 -0500

    Add partial Arcadion M7 presets

--- src/presets/raid/Arcadion.ts
+++ src/presets/raid/Arcadion.ts
@@ -42,4 +42,26 @@ const PRESET_4_PHASE_2: ArenaPreset = {
     backgroundImage: '/arena/arcadion4-p2.svg',
 };
 
-export const ARENA_PRESETS_RAID_ARCADION = [PRESET_2, PRESET_3, PRESET_4, PRESET_4_PHASE_2];
+const PRESET_7: ArenaPreset = {
+    name: 'AAC Cruiserweight M3',
+    spoilerFreeName: 'AAC Cruiserweight M3 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.Rectangular, rows: 4, columns: 4 },
+    backgroundImage: '/arena/arcadion7.svg',
+};
+
+const PRESET_7_PHASE_2: ArenaPreset = {
+    name: 'AAC Cruiserweight M3 (Phase 2)',
+    spoilerFreeName: 'AAC Cruiserweight M3 ████',
+    shape: ArenaShape.Rectangle,
+    width: 300,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.CustomRectangular, rows: [-150, 150], columns: [0] },
+    backgroundImage: '/arena/arcadion7-p2.svg',
+};
+
+export const ARENA_PRESETS_RAID_ARCADION = [PRESET_2, PRESET_3, PRESET_4, PRESET_4_PHASE_2, PRESET_7, PRESET_7_PHASE_2];
