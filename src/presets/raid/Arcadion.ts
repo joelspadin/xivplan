ae7bbd046e674b30ec20bf193d03cb7592c76ac9
Joel Spadin <joelspadin@gmail.com>
2025-06-09 23:30:34 -0500

    Add arena more Arcadion presets

--- src/presets/raid/Arcadion.ts
+++ src/presets/raid/Arcadion.ts
@@ -42,6 +42,61 @@ const PRESET_4_PHASE_2: ArenaPreset = {
     backgroundImage: '/arena/arcadion4-p2.svg',
 };
 
+const PRESET_6: ArenaPreset = {
+    name: 'AAC Cruiserweight M2',
+    spoilerFreeName: 'AAC Cruiserweight M2 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion6.svg',
+};
+
+const PRESET_6_QUICKSAND: ArenaPreset = {
+    name: 'AAC Cruiserweight M2 (Quicksand)',
+    spoilerFreeName: 'AAC Cruiserweight M2 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion6-quicksand.svg',
+};
+
+const PRESET_6_QUICKSAND_2: ArenaPreset = {
+    name: 'AAC Cruiserweight M2 (Quicksand 2)',
+    spoilerFreeName: 'AAC Cruiserweight M2 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion6-quicksand2.svg',
+};
+
+const PRESET_6_RIVER: ArenaPreset = {
+    name: 'AAC Cruiserweight M2 (Riverscape)',
+    spoilerFreeName: 'AAC Cruiserweight M2 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion6-river.svg',
+};
+
+const PRESET_6_VOLCANO: ArenaPreset = {
+    name: 'AAC Cruiserweight M2 (Volcano)',
+    spoilerFreeName: 'AAC Cruiserweight M2 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion6-volcano.svg',
+};
+
 const PRESET_7: ArenaPreset = {
     name: 'AAC Cruiserweight M3',
     spoilerFreeName: 'AAC Cruiserweight M3 ████',
@@ -64,4 +119,64 @@ const PRESET_7_PHASE_2: ArenaPreset = {
     backgroundImage: '/arena/arcadion7-p2.svg',
 };
 
-export const ARENA_PRESETS_RAID_ARCADION = [PRESET_2, PRESET_3, PRESET_4, PRESET_4_PHASE_2, PRESET_7, PRESET_7_PHASE_2];
+const PRESET_7_PHASE_3: ArenaPreset = {
+    name: 'AAC Cruiserweight M3 (Phase 3)',
+    spoilerFreeName: 'AAC Cruiserweight M3 ████',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.Rectangular, rows: 8, columns: 8 },
+    backgroundImage: '/arena/arcadion7-p3.svg',
+};
+
+const PRESET_8: ArenaPreset = {
+    name: 'AAC Cuirserweight M4',
+    spoilerFreeName: 'AAC Cruiserweight M4 ████',
+    shape: ArenaShape.Circle,
+    width: 800,
+    height: 800,
+    padding: 20,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion8.svg',
+};
+
+const PRESET_8_PHASE_2: ArenaPreset = {
+    name: 'AAC Cuirserweight M4 (Phase 2)',
+    spoilerFreeName: 'AAC Cruiserweight M4 ████',
+    shape: ArenaShape.Circle,
+    width: 580,
+    height: 580,
+    padding: DEFAULT_ARENA_PADDING,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion8-p2.svg',
+};
+
+const PRESET_8_SAVAGE_PHASE_2: ArenaPreset = {
+    name: 'AAC Cuirserweight M4 (Savage Phase 2)',
+    spoilerFreeName: 'AAC Cruiserweight M4 ████',
+    shape: ArenaShape.None,
+    width: 840,
+    height: 840,
+    padding: 0,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/arcadion8-sp2.svg',
+};
+
+export const ARENA_PRESETS_RAID_ARCADION = [
+    PRESET_2,
+    PRESET_3,
+    PRESET_4,
+    PRESET_4_PHASE_2,
+    PRESET_6,
+    PRESET_6_QUICKSAND,
+    PRESET_6_QUICKSAND_2,
+    PRESET_6_RIVER,
+    PRESET_6_VOLCANO,
+    PRESET_7,
+    PRESET_7_PHASE_2,
+    PRESET_7_PHASE_3,
+    PRESET_8,
+    PRESET_8_PHASE_2,
+    PRESET_8_SAVAGE_PHASE_2,
+];
