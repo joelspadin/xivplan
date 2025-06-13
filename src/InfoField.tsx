1b8e2b219d0ee6f9bba55493261bb88c387e0d9d
Joel Spadin <joelspadin@gmail.com>
2025-06-12 19:04:21 -0500

    Simplify InfoField

--- src/InfoField.tsx
+++ src/InfoField.tsx
@@ -1,51 +1,37 @@
 import {
+    Field,
     FieldProps,
-    FieldState,
     ForwardRefComponent,
     InfoLabel,
     InfoLabelProps,
-    PopoverSurfaceProps,
-    Slot,
-    renderField_unstable,
-    slot,
-    useFieldContextValues_unstable,
-    useFieldStyles_unstable,
-    useField_unstable,
+    LabelProps,
 } from '@fluentui/react-components';
 import React from 'react';
 
-// TODO: remove this once https://github.com/microsoft/fluentui/issues/31482 is implemented
-// and it is possible to set an InfoLabel in a Field's label slot.
-
 export interface InfoFieldProps extends FieldProps {
-    info?: NonNullable<Slot<ForwardRefComponent<PopoverSurfaceProps>>>;
+    info?: InfoLabelProps['info'];
 }
 
+/**
+ * Shortcut for a <Field> with an <InfoLabel> as its label.
+ */
 export const InfoField: ForwardRefComponent<InfoFieldProps> = React.forwardRef((props, ref) => {
-    const state = useInfoField(props, ref);
-    useFieldStyles_unstable(state);
-    const context = useFieldContextValues_unstable(state);
-    return renderField_unstable(state, context);
+    const { label, info, children, ...rest } = props;
+    return (
+        <Field
+            ref={ref}
+            {...rest}
+            label={{
+                children: (_: unknown, slotProps: LabelProps) => (
+                    <InfoLabel {...slotProps} info={info}>
+                        {label}
+                    </InfoLabel>
+                ),
+            }}
+        >
+            {children}
+        </Field>
+    );
 });
 
 InfoField.displayName = 'InfoField';
-
-const useInfoField = (props: InfoFieldProps, ref: React.Ref<HTMLDivElement>): FieldState => {
-    const { info, ...rest } = props;
-
-    const state = useField_unstable(rest, ref);
-
-    const id = state.label?.id ?? '';
-    const htmlFor = state.label?.htmlFor ?? '';
-
-    const label = slot.optional<InfoLabelProps>(props.label, {
-        defaultProps: { htmlFor, id, info, required: rest.required, size: 'medium' },
-        elementType: InfoLabel,
-    });
-
-    return {
-        ...state,
-        components: { ...state.components, label: InfoLabel },
-        label,
-    };
-};
