bcea26798779a8b6b3b6c8b78be4d9b9a79d4732
Joel Spadin <joelspadin@gmail.com>
2025-06-22 14:42:06 -0500

    Update color picker style
    Moved the color picker button inside the text box, so it is more
    obviously attached to the input field. Also changed the placeholder text
    to #000000 so it isn't redundant with the field label.

--- src/CompactColorPicker.tsx
+++ src/CompactColorPicker.tsx
@@ -1,10 +1,14 @@
 import {
-    ColorSwatch,
     Field,
     Popover,
     PopoverSurface,
     PopoverTrigger,
+    createCustomFocusIndicatorStyle,
+    makeResetStyles,
     makeStyles,
+    mergeClasses,
+    shorthands,
+    swatchCSSVars,
     tokens,
 } from '@fluentui/react-components';
 import React, { useCallback, useState } from 'react';
@@ -33,6 +37,7 @@ export const CompactColorPicker: React.FC<CompactColorPickerProps> = ({
     debounceTime = debounceTime ?? DEFAULT_DEBOUNCE_TIME;
 
     const classes = useStyles();
+    const resetStyles = useResetStyles();
     const [colorValid, setColorValid] = useState(true);
 
     const notifyChanged = useCallback(
@@ -75,43 +80,119 @@ export const CompactColorPicker: React.FC<CompactColorPickerProps> = ({
                 validationMessage={colorValid ? undefined : 'Invalid color'}
                 validationState={colorValid ? 'none' : 'error'}
             >
-                <div className={classes.wrapper}>
-                    <Popover size="small" withArrow>
-                        <PopoverTrigger>
-                            <ColorSwatch size="small" color={color || '#000'} value={color} />
-                        </PopoverTrigger>
-                        <PopoverSurface tabIndex={-1}>
-                            <ColorPicker value={pickerColor} onChange={setPickerColor} />
-                        </PopoverSurface>
-                    </Popover>
-                    <DeferredInput
-                        className={classes.input}
-                        value={color}
-                        onChange={(ev, data) => setColorText(data.value)}
-                        placeholder={placeholder}
-                    />
-                </div>
+                <DeferredInput
+                    contentBefore={{
+                        children: (
+                            <div className={classes.buttonWrapper}>
+                                <Popover size="small" withArrow>
+                                    <PopoverTrigger>
+                                        <button
+                                            className={mergeClasses(resetStyles, classes.button)}
+                                            style={{ [swatchCSSVars.color]: color || '#000' }}
+                                            aria-label="Open color picker"
+                                        />
+                                    </PopoverTrigger>
+                                    <PopoverSurface tabIndex={-1}>
+                                        <ColorPicker value={pickerColor} onChange={setPickerColor} />
+                                    </PopoverSurface>
+                                </Popover>
+                            </div>
+                        ),
+                    }}
+                    className={classes.input}
+                    value={color}
+                    onChange={(ev, data) => setColorText(data.value)}
+                    placeholder={placeholder ?? '#000000'}
+                />
             </Field>
         </>
     );
 };
+
+const useResetStyles = makeResetStyles({
+    display: 'inline-flex',
+    flexShrink: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxSizing: 'border-box',
+    border: `1px solid var(${swatchCSSVars.borderColor})`,
+    background: `var(${swatchCSSVars.color})`,
+    overflow: 'hidden',
+    padding: '0',
+    ':hover': {
+        cursor: 'pointer',
+        border: 'none',
+        boxShadow: `
+            inset 0 0 0 ${tokens.strokeWidthThick} ${tokens.colorBrandStroke1},
+            inset 0 0 0 ${tokens.strokeWidthThicker} ${tokens.colorStrokeFocus1}
+        `,
+    },
+    ':hover:active': {
+        border: 'none',
+        boxShadow: `
+            inset 0 0 0 ${tokens.strokeWidthThicker} ${tokens.colorCompoundBrandStrokePressed},
+            inset 0 0 0 ${tokens.strokeWidthThickest} ${tokens.colorStrokeFocus1}
+        `,
+    },
+    ':focus': {
+        outline: 'none',
+    },
+    ':focus-visible': {
+        outline: 'none',
+    },
+    ...createCustomFocusIndicatorStyle({
+        border: 'none',
+        outline: 'none',
+        boxShadow: `
+            inset 0 0 0 ${tokens.strokeWidthThick} ${tokens.colorStrokeFocus2},
+            inset 0 0 0 ${tokens.strokeWidthThicker} ${tokens.colorStrokeFocus1}
+        `,
+    }),
+
+    // High contrast styles
+
+    '@media (forced-colors: active)': {
+        forcedColorAdjust: 'none',
+        ':hover': {
+            boxShadow: `
+                inset 0 0 0 ${tokens.strokeWidthThick} ${tokens.colorBrandStroke2Hover},
+                inset 0 0 0 ${tokens.strokeWidthThicker} ${tokens.colorStrokeFocus1}
+            `,
+        },
+        ':hover:active': {
+            boxShadow: `
+                inset 0 0 0 ${tokens.strokeWidthThick} ${tokens.colorBrandStroke2Pressed},
+                inset 0 0 0 ${tokens.strokeWidthThicker} ${tokens.colorStrokeFocus1}
+            `,
+        },
+    },
+});
 
 const useStyles = makeStyles({
     field: {
         flex: 1,
     },
 
-    wrapper: {
-        display: 'flex',
-        flexFlow: 'row',
-        alignItems: 'center',
-        gap: tokens.spacingHorizontalS,
-        marginInlineStart: tokens.spacingHorizontalXS,
+    input: {
+        paddingLeft: 0,
+        overflow: 'hidden',
     },
 
-    input: {
-        minWidth: 'auto',
-        width: 0,
-        flex: 1,
+    buttonWrapper: {
+        display: 'flex',
+        flexFlow: 'row',
+        boxSizing: 'border-box',
+        width: '30px',
+        height: '30px',
+        padding: '2px',
+        marginRight: tokens.spacingHorizontalXS,
+    },
+
+    button: {
+        width: '100%',
+        height: '100%',
+        marginRight: tokens.spacingHorizontalXS,
+        borderRadius: tokens.borderRadiusMedium,
+        ...shorthands.border(tokens.strokeWidthThin, 'solid', tokens.colorNeutralStroke3),
     },
 });
