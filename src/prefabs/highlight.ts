f21ccef7bfbde289546ddc7630db6d750df30223
Aygox <155141069+Aygox-XIV@users.noreply.github.com>
2025-12-23 16:16:40 -0800

    Add dashed borders to objects connected to selected or spotlight objects.
    Also fix linter errors

--- src/prefabs/highlight.ts
+++ src/prefabs/highlight.ts
@@ -1,9 +1,10 @@
 import { ShapeConfig } from 'konva/lib/Shape';
 import { EditMode } from '../editMode';
 import { isMoveable, UnknownObject } from '../scene';
+import { getObjectById, useScene } from '../SceneProvider';
 import { useSelection, useSpotlight } from '../selection';
 import { SceneSelection } from '../SelectionContext';
-import { SELECTED_PROPS, SPOTLIGHT_PROPS } from '../theme';
+import { SELECTED_CONNECTED_PROPS, SELECTED_PROPS, SPOTLIGHT_CONNECTED_PROPS, SPOTLIGHT_PROPS } from '../theme';
 import { useEditMode } from '../useEditMode';
 
 function shouldShowResizer(object: UnknownObject, selection: SceneSelection, editMode: EditMode) {
@@ -20,13 +21,33 @@ export function useHighlightProps(object: UnknownObject): ShapeConfig | undefine
     const [editMode] = useEditMode();
     const [selection] = useSelection();
     const [spotlight] = useSpotlight();
+    const scene = useScene().scene;
 
     // Regular selection styling takes precedence if it's the only selected object.
     if (spotlight.has(object.id) && !(selection.size === 1 && selection.has(object.id))) {
         return SPOTLIGHT_PROPS;
     }
 
-    return selection.has(object.id) && !shouldShowResizer(object, selection, editMode) ? SELECTED_PROPS : undefined;
+    if (selection.has(object.id) && !shouldShowResizer(object, selection, editMode)) {
+        return SELECTED_PROPS;
+    }
+
+    // If one of the objects this is connected to is in the spotlight or selection
+    let parentId = isMoveable(object) ? object.parentId : undefined;
+    while (parentId) {
+        if (spotlight.has(parentId)) {
+            // This is slightly weird when the parent object has a resizer thing.
+            // maybe the resizer should follow the spotlight coloring?
+            return SPOTLIGHT_CONNECTED_PROPS;
+        }
+        if (selection.has(parentId)) {
+            return SELECTED_CONNECTED_PROPS;
+        }
+        const parentObject = getObjectById(scene, parentId);
+        parentId = isMoveable(parentObject) ? parentObject.parentId : undefined;
+    }
+
+    return undefined;
 }
 
 export function useShowResizer(object: UnknownObject): boolean {
