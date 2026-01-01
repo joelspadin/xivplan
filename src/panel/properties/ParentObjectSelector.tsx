35a4175842074026510694af97919797391c9d63
Aygox <155141069+Aygox-XIV@users.noreply.github.com>
2026-01-01 13:53:29 -0800

    Extract the parent object selector into a shared component.

---
+++ src/panel/properties/ParentObjectSelector.tsx
@@ -0,0 +1,172 @@
+import { SplitButton, Tooltip } from '@fluentui/react-components';
+import { bundleIcon, DismissFilled, DismissRegular, LinkRegular } from '@fluentui/react-icons';
+import React from 'react';
+import { getAllowedPositionParentIds, getAllowedRotationParentIds } from '../../connections';
+import { getAbsolutePosition, getAbsoluteRotation } from '../../coord';
+import { EditMode } from '../../editMode';
+import { ConnectionType } from '../../EditModeContext';
+import { isMoveable, isRotateable, SceneObject } from '../../scene';
+import { getObjectById, useScene } from '../../SceneProvider';
+import { selectNone, selectSingle, useSelection, useSpotlight } from '../../selection';
+import { useConnectionSelection } from '../../useConnectionSelection';
+import { useEditMode } from '../../useEditMode';
+import { commonValue, omit } from '../../util';
+import { getListComponent } from '../ListComponentRegistry';
+
+export interface ParentObjectSelectorProps {
+    readonly connectionType: ConnectionType;
+    readonly objects: readonly SceneObject[];
+}
+
+export const ParentObjectSelector: React.FC<ParentObjectSelectorProps> = ({ connectionType, objects }) => {
+    const { scene, step, dispatch } = useScene();
+    const [, setSpotlight] = useSpotlight();
+    const [, setSelection] = useSelection();
+    const [, setEditMode] = useEditMode();
+    const [, setConnectionSelection] = useConnectionSelection();
+
+    const getParentId = (obj: SceneObject) => {
+        if (isMoveable(obj) && connectionType == ConnectionType.POSITION) {
+            return obj.parentId;
+        }
+        if (isRotateable(obj) && connectionType == ConnectionType.ROTATION) {
+            return obj.facingId;
+        }
+    };
+    const commonParentId = commonValue(objects, getParentId);
+    const haveSharedLink = commonParentId !== undefined;
+    const haveAnyLink = objects.find((obj) => getParentId(obj) !== undefined) !== undefined;
+
+    let allowedParentIds: number[];
+    switch (connectionType) {
+        case ConnectionType.POSITION:
+            allowedParentIds = getAllowedPositionParentIds(step, objects);
+            break;
+        case ConnectionType.ROTATION:
+            allowedParentIds = getAllowedRotationParentIds(step, objects);
+            break;
+        default:
+            allowedParentIds = [];
+    }
+
+    const parentObject = haveSharedLink && getObjectById(scene, commonParentId);
+    const ParentDisplayComponent = parentObject && getListComponent(parentObject);
+
+    const onMouseEnterParent = () => {
+        setSpotlight(haveSharedLink ? selectSingle(commonParentId) : selectNone());
+    };
+    const onMouseLeaveParent = () => {
+        setSpotlight(selectNone());
+    };
+
+    function onClickParent(event: React.MouseEvent<HTMLElement, MouseEvent>): void {
+        if (haveSharedLink) {
+            // Don't trigger the selection if modifiers are held
+            if (!event.ctrlKey && !event.shiftKey) {
+                setSelection(selectSingle(commonParentId));
+                // The element disappearing will not trigger an onMouseLeave
+                setSpotlight(selectNone());
+            }
+            return;
+        } else {
+            // For both "no current connections" and ""
+            if (allowedParentIds.length == 0) {
+                return;
+            }
+            setEditMode(EditMode.SelectConnection);
+            setConnectionSelection({
+                objectIdsToConnect: objects.map((obj) => obj.id),
+                connectionType,
+            });
+        }
+    }
+    function onClickUnlink(): void {
+        if (haveAnyLink) {
+            switch (connectionType) {
+                case ConnectionType.POSITION:
+                    dispatch({
+                        type: 'update',
+                        value: objects.filter(isMoveable).map((obj) => {
+                            const absolutePos = getAbsolutePosition(scene, obj);
+                            return {
+                                ...omit(obj, 'parentId'),
+                                ...absolutePos,
+                            };
+                        }),
+                    });
+                    break;
+                case ConnectionType.ROTATION:
+                    dispatch({
+                        type: 'update',
+                        value: objects.filter(isRotateable).map((obj) => {
+                            return {
+                                ...omit(obj, 'facingId'),
+                                rotation: isMoveable(obj) ? getAbsoluteRotation(scene, obj) : 0,
+                            };
+                        }),
+                    });
+                    break;
+            }
+        }
+    }
+
+    let newLinkText: string;
+    let newLinkTooltip: string;
+    let currentLinkTooltip: string;
+    switch (connectionType) {
+        case ConnectionType.POSITION:
+            newLinkText = 'Link position';
+            newLinkTooltip =
+                allowedParentIds.length == 0
+                    ? 'No available object to attach to'
+                    : 'Attach the selection to another object';
+            currentLinkTooltip = 'If this object moves, so does the selection';
+            break;
+        case ConnectionType.ROTATION:
+            newLinkText = 'Face object';
+            newLinkTooltip =
+                allowedParentIds.length == 0
+                    ? 'No available object to face'
+                    : 'Automatically rotate towards another object';
+            currentLinkTooltip = 'If this object moves, the selection will rotate to face it (plus the Rotation value)';
+            break;
+    }
+
+    return (
+        <SplitButton
+            icon={<LinkRegular />}
+            menuIcon={<UnlinkButton />}
+            appearance="subtle"
+            primaryActionButton={{
+                onClick: onClickParent,
+                onMouseEnter: onMouseEnterParent,
+                onMouseLeave: onMouseLeaveParent,
+                disabled: !haveAnyLink && allowedParentIds.length == 0,
+            }}
+            menuButton={{ onClick: onClickUnlink, disabled: !haveAnyLink }}
+        >
+            {haveSharedLink && ParentDisplayComponent && (
+                <Tooltip content={currentLinkTooltip} relationship="description">
+                    {
+                        // https://github.com/facebook/react/issues/34794
+                        // eslint-disable-next-line react-hooks/static-components
+                        <ParentDisplayComponent isNested={true} object={parentObject} />
+                    }
+                </Tooltip>
+            )}
+            {haveSharedLink && !ParentDisplayComponent && 'Unknown object'}
+            {!haveSharedLink && haveAnyLink && 'Multiple objects'}
+            {!haveAnyLink && (
+                <Tooltip content={newLinkTooltip} relationship="description">
+                    <div>{newLinkText}</div>
+                </Tooltip>
+            )}
+        </SplitButton>
+    );
+};
+
+const UnlinkIcon = bundleIcon(DismissFilled, DismissRegular);
+
+const UnlinkButton: React.FC = () => {
+    return <UnlinkIcon />;
+};
--- src/panel/properties/PositionControl.tsx
+++ src/panel/properties/PositionControl.tsx
@@ -1,37 +1,19 @@
 import { Field, ToggleButton, Tooltip } from '@fluentui/react-components';
-import {
-    LinkAddRegular,
-    LinkDismissRegular,
-    LinkMultipleRegular,
-    LinkRegular,
-    LockClosedRegular,
-    LockMultipleRegular,
-    LockOpenRegular,
-} from '@fluentui/react-icons';
+import { LinkRegular, LockClosedRegular, LockMultipleRegular, LockOpenRegular } from '@fluentui/react-icons';
 import React from 'react';
 import { ConnectionType } from '../../EditModeContext';
-import { getObjectById, useScene } from '../../SceneProvider';
+import { useScene } from '../../SceneProvider';
 import { SpinButton } from '../../SpinButton';
-import { getAllowedPositionParentIds } from '../../connections';
-import { getAbsolutePosition } from '../../coord';
-import { EditMode } from '../../editMode';
 import { useSpinChanged } from '../../prefabs/useSpinChanged';
 import { MoveableObject } from '../../scene';
-import { selectNone, selectSingle, useSelection, useSpotlight } from '../../selection';
-import { useConnectionSelection } from '../../useConnectionSelection';
 import { useControlStyles } from '../../useControlStyles';
-import { useEditMode } from '../../useEditMode';
-import { commonValue, omit, setOrOmit } from '../../util';
-import { getListComponent } from '../ListComponentRegistry';
+import { commonValue, setOrOmit } from '../../util';
 import { PropertiesControlProps } from '../PropertiesControl';
+import { ParentObjectSelector } from './ParentObjectSelector';
 
 export const PositionControl: React.FC<PropertiesControlProps<MoveableObject>> = ({ objects }) => {
     const classes = useControlStyles();
-    const { scene, step, dispatch } = useScene();
-    const [, setEditMode] = useEditMode();
-    const [, setConnectionSelection] = useConnectionSelection();
-    const [, setSpotlight] = useSpotlight();
-    const [, setSelection] = useSelection();
+    const { dispatch } = useScene();
 
     const x = commonValue(objects, (obj) => obj.x);
     const y = commonValue(objects, (obj) => obj.y);
@@ -49,68 +31,8 @@ export const PositionControl: React.FC<PropertiesControlProps<MoveableObject>> =
         dispatch({ type: 'update', value: objects.map((obj) => ({ ...obj, y })) }),
     );
 
-    const allowedParentIds = getAllowedPositionParentIds(step, objects);
-
-    const onToggleLinked = () => {
-        if (currentlyLinked) {
-            dispatch({
-                type: 'update',
-                value: objects.map((obj) => {
-                    const absolutePos = getAbsolutePosition(scene, obj);
-                    return {
-                        ...omit(obj, 'parentId'),
-                        ...absolutePos,
-                    };
-                }),
-            });
-        } else {
-            if (allowedParentIds.length == 0) {
-                return;
-            }
-            setEditMode(EditMode.SelectConnection);
-            setConnectionSelection({
-                objectIdsToConnect: objects.map((obj) => obj.id),
-                connectionType: ConnectionType.POSITION,
-            });
-        }
-    };
-
     const icon = pinned === undefined ? <LockMultipleRegular /> : pinned ? <LockClosedRegular /> : <LockOpenRegular />;
     const tooltip = pinned ? 'Unlock position' : 'Lock position';
-    const linkedTooltip =
-        allowedParentIds.length == 0
-            ? 'No available link targets'
-            : currentlyLinked
-              ? 'Unlink position'
-              : 'Link position';
-
-    const linkIcon = currentlyLinked ? (
-        objects.length == 1 ? (
-            <LinkAddRegular />
-        ) : (
-            <LinkMultipleRegular />
-        )
-    ) : (
-        <LinkDismissRegular />
-    );
-
-    const parentObject = parentId && getObjectById(scene, parentId);
-    const ParentDisplayComponent = parentObject && getListComponent(parentObject);
-
-    const onMouseEnterParent = () => {
-        setSpotlight(parentId === undefined ? selectNone() : selectSingle(parentId));
-    };
-    const onMouseLeaveParent = () => {
-        setSpotlight(selectNone());
-    };
-    function onClickParent(event: React.MouseEvent<HTMLDivElement, MouseEvent>): void {
-        // Don't trigger the selection if modifiers are held
-        if (parentId !== undefined && !event.ctrlKey && !event.shiftKey) {
-            setSelection(selectSingle(parentId));
-            // The element disappearing will not trigger an onMouseLeave
-            setSpotlight(selectNone());
-        }
-    }
 
     return (
         <>
@@ -124,31 +46,9 @@ export const PositionControl: React.FC<PropertiesControlProps<MoveableObject>> =
                 <Tooltip content={tooltip} relationship="label" withArrow>
                     <ToggleButton checked={pinned || false} onClick={onTogglePinned} icon={icon} />
                 </Tooltip>
-                <Tooltip content={linkedTooltip} relationship="label" withArrow>
-                    <ToggleButton
-                        checked={parentId !== undefined}
-                        onClick={onToggleLinked}
-                        icon={linkIcon}
-                        disabled={allowedParentIds.length == 0}
-                    />
-                </Tooltip>
             </div>
             <div className={classes.row}>
-                {ParentDisplayComponent && (
-                    <Field
-                        label="Position is relative to:"
-                        onMouseEnter={onMouseEnterParent}
-                        onMouseLeave={onMouseLeaveParent}
-                        onClick={onClickParent}
-                    >
-                        {
-                            // (not really nested, but it removes the visiblity toggle and deletion button, and a smaller size is OK)
-                            // https://github.com/facebook/react/issues/34794
-                            // eslint-disable-next-line react-hooks/static-components
-                            <ParentDisplayComponent isNested={true} object={parentObject} />
-                        }
-                    </Field>
-                )}
+                <ParentObjectSelector objects={objects} connectionType={ConnectionType.POSITION} />
             </div>
         </>
     );
@@ -160,14 +60,15 @@ interface PositionLabelProps {
 }
 
 const PositionLabel: React.FC<PositionLabelProps> = ({ coordinate, currentlyLinked }) => {
+    const classes = useControlStyles();
     return (
-        <>
+        <span className={classes.label}>
             {coordinate}
             {currentlyLinked && (
                 <Tooltip content="Relative to the object below" relationship="description">
-                    <LinkRegular style={{ paddingLeft: '5px' }} />
+                    <LinkRegular />
                 </Tooltip>
             )}
-        </>
+        </span>
     );
 };
